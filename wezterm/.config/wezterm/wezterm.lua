17d49e5e8ba79040dabb6fd37edf8bc31be5f16f
Tanner Leonard <tannerljosephdfs@gmail.com>
2026-01-07 18:19:12 -0700

    added keymaps for smart splits

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,6 +7,44 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 13.6
 
+-- Smart Splits Config
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+-- End Smart Splits Config
+
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
 
 config.keys = {
@@ -14,6 +52,11 @@ config.keys = {
 		key = "|",
 		mods = "LEADER|SHIFT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 	-- Send "CTRL-B" to the terminal when pressing CTRL-B, CTRL-B
 	{
@@ -63,6 +106,21 @@ config.keys = {
 		mods = "LEADER",
 		action = wezterm.action.ShowTabNavigator,
 	},
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 }
 
 config.max_fps = 240
